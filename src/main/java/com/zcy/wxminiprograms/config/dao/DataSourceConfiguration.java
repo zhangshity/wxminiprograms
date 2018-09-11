08f6940c93cd9e2b7e4085af943a07954a3283be
chunyang.zhang <525768181@qq.com>
2018-09-11 16:17:28 +0800

    dao层的配置和编写。mapper接口,mapper.xml。dataSourceConfig,sessionFactoryConfig

--- src/main/java/com/zcy/wxminiprograms/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/zcy/wxminiprograms/config/dao/DataSourceConfiguration.java
@@ -1,26 +1,44 @@
 package com.zcy.wxminiprograms.config.dao;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.beans.PropertyVetoException;
 
+@Configuration
+@MapperScan("com.zcy.wxminiprograms.dao")//配置mybatis mapper扫描路径
 public class DataSourceConfiguration {
-    
-    private String jdbcUrl;
+
+    @Value("${spring.datasource.driver-class-name}")
     private String jdbcDriver;
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+    @Value("${spring.datasource.username}")
     private String jdbcUsername;
+    @Value("${spring.datasource.password}")
     private String jdbcPassword;
 
+    //Spring IOC容器 管理 dataSource(注解方式)
     @Bean(name = "dataSource")
     public ComboPooledDataSource createDataSource() throws PropertyVetoException {
+
+        //创建ComboPooledDataSource对象(存储数据库连接信息)
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        //jdbc驱动
         dataSource.setDriverClass(jdbcDriver);
+        //数据库url
         dataSource.setJdbcUrl(jdbcUrl);
+        //数据库 用户名
         dataSource.setUser(jdbcUsername);
+        //数据库 密码
         dataSource.setPassword(jdbcPassword);
-        //关闭连接后不自动commit
+        //自动提交关闭。(关闭连接后,数据库不自动commit)
         dataSource.setAutoCommitOnClose(false);
+
         return dataSource;
     }
 }
