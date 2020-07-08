5bdb42e254c7fbd47ea4e23bd3b58668f93fac1d
chunyang.zhang <525768181@qq.com>
2020-07-08 16:37:19 +0800

    测试demo类改包名

--- src/main/java/com/zcy/wxminiprograms/_hello/Hello.java
+++ src/main/java/com/zcy/wxminiprograms/_hello/Hello.java
@@ -1,8 +1,7 @@
-package com.zcy.wxminiprograms._hello_demo_test;
+package com.zcy.wxminiprograms._hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  //处理http请求,返回json格式
